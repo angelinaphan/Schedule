0703eb85737379c193746b09dbc43af9406fd41e
angelinaphan <91085907+angelinaphan@users.noreply.github.com>
2024-03-08 11:49:26 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ $(document).ready(function () {
 
   $("p").click(function(){
     // action goes here!!
-  });
+  });                              
 
 // https://stackoverflow.com/questions/56822076/fetching-json-data-to-html-table-onclick-button
+// https://www.geeksforgeeks.org/jquery-getjson-method/
