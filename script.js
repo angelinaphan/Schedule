a8fdd0944a81d21a818625ef70d177e28a422ac5
angelinaphan <91085907+angelinaphan@users.noreply.github.com>
2024-03-12 09:56:38 -0400

    link

--- script.js
+++ script.js
@@ -1,59 +1,25 @@
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
+  document.getElementById("myDropdown").classList.toggle("show");
+}
 
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
-  };
+  }
+};
 
-  // A DAY
-  $(document).ready(function () { 
-
-    // FETCHING DATA FROM JSON FILE 
-    $.getJSON("schedule.json",  
-            function (data) { 
-        var student = ''; 
-
-        // ITERATING THROUGH OBJECTS 
-        $.each(data, function (key, value) { 
-
-            //CONSTRUCTION OF ROWS HAVING 
-            // DATA FROM JSON OBJECT 
-            student += '<tr>'; 
-            student += '<td>' +  
-                value.Class_Name + '</td>'; 
-
-            student += '<td>' +  
-                value.Teacher_Name + '</td>'; 
-
-            student += '<td>' +  
-                value.Room_Number + '</td>'; 
-
-            student += '<td>' +  
-                value.Days + '</td>'; 
-
-            student += '</tr>'; 
-        }); 
-
-        //INSERTING ROWS INTO TABLE  
-        $('#table').append(student); 
-    }); 
-}); 
-
-// B DAY 
+// A DAY
 $(document).ready(function () { 
 
   // FETCHING DATA FROM JSON FILE 
-  $.getJSON("scheduleB.json",  
+  $.getJSON("schedule.json",  
           function (data) { 
       var student = ''; 
 
@@ -79,6 +45,9 @@ $(document).ready(function () {
       }); 
 
       //INSERTING ROWS INTO TABLE  
-      $('#table1').append(student); 
+      $('#table').append(student); 
   }); 
 }); 
+
+// B DAY 
+// https://jsfiddle.net/amsv/15h74uy6/
