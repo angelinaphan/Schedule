342267c20191a23d2610aaf485108becbc544985
angelinaphan <91085907+angelinaphan@users.noreply.github.com>
2024-03-14 15:16:28 -0400

    table

--- script.js
+++ script.js
@@ -9,3 +9,27 @@ function toggleTable() {
     document.getElementById("myTable1").classList.toggle("hidden");
   
   };
+
+  function toggleTable3() {
+
+    document.getElementById("myTable3").classList.toggle("hidden");
+  
+  };
+
+  function toggleTable4() {
+
+    document.getElementById("myTable4").classList.toggle("hidden");
+  
+  };
+
+  function toggleTable5() {
+
+    document.getElementById("myTable5").classList.toggle("hidden");
+  
+  };
+
+  function toggleTable6() {
+
+    document.getElementById("myTable6").classList.toggle("hidden");
+  
+  };
